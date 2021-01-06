cb587866ca7e955744576811ca3dda45f226a849
184645L <184645L@CL1PC06.SEGSTUD.NYP.EDU.SG>
2021-01-06 10:39:57 +0800

    search servlet

--- src/java/jeb/SearchServlet.java
+++ src/java/jeb/SearchServlet.java
@@ -1,5 +1,16 @@
 package jeb;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +21,25 @@ package jeb;
  *
  * @author 184645L
  */
-public class SearchServlet {
-    
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet {
+    //Inject your EJB here
+    @EJB
+    private SearchBean bookManagementBean;
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException{
+        String searchTerm = request.getParameter("searchterm");
+        List<Products> searchResult = new ArrayList<>();
+        searchResult = bookManagementBean.searchBook(searchTerm);
+        
+        //Set the search term and the search results in the session
+        HttpSession session = request.getSession();
+        session.setAttribute("searchterm", searchTerm);
+        session.setAttribute("searchresult", (Object)searchResult);
+        
+        //Client side redirect
+        response.sendRedirect(this.getServletContext().getContextPath() + "/searchresults.jsp"); 
+    }
 }
